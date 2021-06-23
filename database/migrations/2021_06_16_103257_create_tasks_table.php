cc5efe694441db87e9bc0101554a30986e68a56a
tatsuki okuda <t-okuda@lotus-gr.co.jp>
2021-06-23 12:48:23 +0900

    commit

--- database/migrations/2021_06_16_103257_create_tasks_table.php
+++ database/migrations/2021_06_16_103257_create_tasks_table.php
@@ -14,7 +14,6 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-           
             $table->bigIncrements('id');
             $table->string('title', 100);
             $table->string('content', 100);
